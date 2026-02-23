4ee59ea5a70562e92b4092a8d4d389bd9c08869f
n.y-group-app <work.kirill.it@gmail.com>
2026-02-23 14:09:16 +0500

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Vector3 velocity;
+    Rigidbody playerRigidbody;
+
+    void Start () {
+        playerRigidbody = GetComponent<Rigidbody> ();
+    }
+
+    public void Move (Vector3 _velocity) {
+        velocity = _velocity;
+    }
+
+    public void LookAt (Vector3 lookPoint) {
+        Vector3 heightCorrectedPoint = new Vector3 (lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt (heightCorrectedPoint);
+    }
+
+    void FixedUpdate () {
+        playerRigidbody.MovePosition (playerRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
